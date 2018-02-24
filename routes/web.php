d9c352e4ee0c34bc00ee747ed4aaf7ee5b57680a
ychakola <yc566@njit.edu>
2018-02-24 13:19:23 -0500

    Basic structure of assignment ready:2

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.home');
+});
+Route::get('/about', function () {
+    return view('pages.about');
+});
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+Route::post('/contact', function () {
+
+
+    // dd(request());
+
+    $data = request()->all();
+
+    //dd($data);
+
+    echo "Email: " .  $data['email'] . '<br>';
+    echo "Body: " . $data['body'];
+
+});
+
+
